ff3588bd88a7b92ce40e4c5aa2d1a6e5c0054c9e
Leul <leulassefa255@gmail.com>
2024-02-04 11:17:39 -0800

    AirBnB clone  RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
