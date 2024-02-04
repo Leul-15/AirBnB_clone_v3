f97837d21bb2a051eaea1d660a93cd9e0e290852
Leul <leulassefa255@gmail.com>
2024-02-04 11:07:16 -0800

    AirBnB clone  RESTful API

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,13 +6,14 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def teardown(self):
+def teardown_appcontext(code):
     """Closes storage"""
     storage.close()
 
@@ -20,12 +21,8 @@ def teardown(self):
 @app.errorhandler(404)
 def not_found(error):
     """404 error"""
-    return make_response(jsonify({"error": "Not found"}), 404)
-
-
-app.register_blueprint(app_views)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", default="0.0.0.0")
-    port = os.getenv("HBNB_API_PORT", default=5000)
-    app.run(debug=True, host=host, port=port, threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
