4385ef53f59e01ac79a4383bbbfa93c627252979
Leul <leulassefa255@gmail.com>
2024-02-05 11:26:48 -0800

    AirBnB clone  RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,23 +3,31 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
-@app_views.route('/status', methods=["GET"])
+@app_views.route("/status", methods=["GET"])
 def status():
-    """return status"""
+    """"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=["GET"])
+@app_views.route("/stats", methods=["GET"])
 def stats():
-    """Return count of each classes"""
+    """"""
     classes = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-        }
-    return jsonify(classes)
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User,
+    }
+    return jsonify(
+        {key: storage.count(value) for key, value in classes.items()}
+    )
