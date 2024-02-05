0dfc57765e1073f891287d26e8769c666e45f7f1
Leul <leulassefa255@gmail.com>
2024-02-05 13:34:34 -0800

    AirBnB clone  RESTful API

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,7 +8,8 @@ from models.city import City
 from models.user import User
 
 
-@app_views.route("/cities/<city_id>/places", methods=["GET"])
+@app_views.route("/cities/<city_id>/places", methods=["GET"],
+                 strict_slashes=False)
 def places(city_id):
     """Retrieves the list of all Place objects of a City"""
     city = storage.get(City, city_id)
@@ -20,7 +21,8 @@ def places(city_id):
     return jsonify(places)
 
 
-@app_views.route("/places/<place_id>", methods=["GET"])
+@app_views.route("/places/<place_id>", methods=["GET"],
+                 strict_slashes=False)
 def place(place_id):
     """Retrieves a Place object"""
     place = storage.get(Place, place_id)
@@ -29,7 +31,8 @@ def place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route("/places/<place_id>", methods=["DELETE"])
+@app_views.route("/places/<place_id>", methods=["DELETE"],
+                 strict_slashes=False)
 def delete_place(place_id):
     """Deletes a Place object"""
     place = storage.get(Place, place_id)
@@ -40,7 +43,8 @@ def delete_place(place_id):
     return jsonify({}), 200
 
 
-@app_views.route("/cities/<city_id>/places", methods=["POST"])
+@app_views.route("/cities/<city_id>/places", methods=["POST"],
+                 strict_slashes=False)
 def create_place(city_id):
     """"""
     city = storage.get(City, city_id)
@@ -62,7 +66,8 @@ def create_place(city_id):
     return jsonify(place.to_dict()), 201
 
 
-@app_views.route("/places/<place_id>", methods=["PUT"])
+@app_views.route("/places/<place_id>", methods=["PUT"],
+                 strict_slashes=False)
 def update_place(place_id):
     """"""
     place = storage.get(Place, place_id)
--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,7 +8,8 @@ from models.review import Review
 from models.user import User
 
 
-@app_views.route("/places/<place_id>/reviews", methods=["GET"])
+@app_views.route("/places/<place_id>/reviews", methods=["GET"],
+                 strict_slashes=False)
 def get_reviews(place_id):
     """Retrieves the list of all Review objects of a Place"""
     place = storage.get(Place, place_id)
@@ -20,7 +21,8 @@ def get_reviews(place_id):
     return jsonify(reviews)
 
 
-@app_views.route("/reviews/<review_id>", methods=["GET"])
+@app_views.route("/reviews/<review_id>", methods=["GET"],
+                 strict_slashes=False)
 def get_review(review_id):
     """Retrieves a Review object"""
     review = storage.get(Review, review_id)
@@ -29,7 +31,8 @@ def get_review(review_id):
     return jsonify(review.to_dict())
 
 
-@app_views.route("/reviews/<review_id>", methods=["DELETE"])
+@app_views.route("/reviews/<review_id>", methods=["DELETE"],
+                 strict_slashes=False)
 def delete_review(review_id):
     """Deletes a Review object"""
     review = storage.get(Review, review_id)
@@ -40,7 +43,8 @@ def delete_review(review_id):
     return jsonify({}), 200
 
 
-@app_views.route("/places/<place_id>/reviews", methods=["POST"])
+@app_views.route("/places/<place_id>/reviews", methods=["POST"],
+                 strict_slashes=False)
 def create_review(place_id):
     """"""
     place = storage.get(Place, place_id)
@@ -62,7 +66,8 @@ def create_review(place_id):
     return make_response(jsonify(review.to_dict()), 201)
 
 
-@app_views.route("/reviews/<review_id>", methods=["PUT"])
+@app_views.route("/reviews/<review_id>", methods=["PUT"],
+                 strict_slashes=False)
 def update_review(review_id):
     """"""
     review = storage.get(Review, review_id)
